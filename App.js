cc2c67305fd281f10b01a93c7ae0478a933106de
Aditya783123 <1051984@etusd.org>
2021-06-03 20:04:44 -0700

    Aditya-Made a working HomeScreen

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import HomeScreen from './screens/HomeScreen'
+import IssLocationScreen from './screens/IssLocationScreen'
+import UpdateScreen from './screens/UpdateScreen'
+import MeteorScreen from './screens/MeteorScreen'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator intialRouteName="HomeScreen" screenOptions={{headerShown:false}}>
+        <Stack.Screen name="HomeScreen" component={HomeScreen}/>
+        <Stack.Screen name="IssLocationScreen" component={IssLocationScreen}/>
+        <Stack.Screen name="MeteorScreen" component={MeteorScreen}/>
+        <Stack.Screen name="UpdateScreen" component={UpdateScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/IssLocationScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class IssLocationScreen extends React.Component{
+    render(){
+        return(
+            <View style = {{flex:1, justifyContent:'center',alignItems:'center'}}> 
+            <Text>IssLocationScreen</Text>
+            </View>
+        )
+    }
+}
